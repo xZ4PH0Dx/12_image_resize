432a78ed3a1e81994c8f01cf64382a9e9733e35c
Z4PH0D <dreamfall3r@gmail.com>
2018-04-22 22:59:08 +0200

    Определил основные функции

--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,33 @@
+from PIL import Image
+import argparse
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def open_image(path_to_original):
+    return Image.open(path_to_original)
 
+
+def resize_image(image, width, height):
+    return image.resize(width, height)
+
+
+def save_image(image, path_to_result):
+    image.save(path_to_result)
+
+
+# def resize_image(path_to_original, path_to_result):
+#    pass
+
+# filepath +
+# width
+# height
+# scale
+# output
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-in', required=True, help='input path')
+    parser.add_argument('-w', help='Width')
+    parser.add_argument('-h', help='Height')
+    parser.add_argument('-s', help='Scale')
+    parser.add_argument('-o', help='Output file')
+    p = parser.parse_args()
