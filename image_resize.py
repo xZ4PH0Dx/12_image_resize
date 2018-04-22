24ce79605a4091aa5d9637dd5488d5cec5bce09f
Z4PH0D <dreamfall3r@gmail.com>
2018-04-23 02:03:15 +0300

    Сохраню, чтобы смог работать с вм

--- image_resize.py
+++ image_resize.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import os
 import sys
 import argparse
 
@@ -24,8 +25,17 @@ def createParser(*args):
     parser.add_argument('-o', '--output', help='Output file')
     return parser.parse_args()
 
-# def resize_image(path_to_original, path_to_result):
-#    pass
+def get_output_path(input_path, suffix):
+    basepath = os.path.splitext(input_path)[0]
+    extention = os.path.splitext(input_path)[1]
+    return basepath + suffix + extention
+
+
+def get_param_value(param_name):
+    if args.param_name:
+        return args.param_name
+    else:
+        return None
 
 # filepath +
 # width
@@ -37,5 +47,10 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         sys.exit('Необходимо указать как минимум путь к файлу')
     args = createParser(sys.argv[1:])
-
-
+    input_path = args.input
+    width = get_param_value(width)
+    if args.output:
+        output_path = args.output
+    else:
+        output_path = get_output_path(input_path, '__{0}X{1}'.format(width,height))
+    print(width)
