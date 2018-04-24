d8e510cf3bc51609925da726bdad9ea4df856dcf
Z4PH0D <dreamfall3r@gmail.com>
2018-04-24 17:05:07 +0200

    Добавил отказоустойчивость

--- image_resize.py
+++ image_resize.py
@@ -42,50 +42,57 @@ def createParser(*args):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        sys.exit('Необходимо указать как минимум путь к файлу')
+        sys.exit('Необходимо указать как минимум путь к файлу!')
     args = createParser(sys.argv[1:])
     input_path = args.input
-    opened_image = open_image(input_path)
+    try:
+        opened_image = open_image(input_path)
+    except (OSError, FileNotFoundError):
+        sys.exit('Не найден файл с изображением!')
     original_width, original_height = get_image_size(opened_image)
     arg_width = args.width
     arg_height = args.height
     arg_scale = args.scale
     proportion = get_proportions(original_width, original_height)
 
-    if arg_scale:
-        arg_scale = float(arg_scale)
-        if arg_width or arg_height:
-            sys.exit('Необходимо указать либо масштаб либо высоту и\или ширину')
-        else:
-            width = int(original_width) * arg_scale
-            height = int(original_height) * arg_scale
+    try:
+        if arg_scale:
+            arg_scale = float(arg_scale)
+            if arg_width or arg_height:
+                sys.exit('Укажите либо масштаб либо высоту и\или ширину!')
+            else:
+                width = int(original_width) * arg_scale
+                height = int(original_height) * arg_scale
 
-    if arg_width and arg_height:
-        width = int(arg_width)
-        height = int(arg_height)
-        if get_proportions(width, height) != proportion:
-            print('Пропорции отличны от оригинала!')
+        elif arg_width and arg_height:
+            width = int(arg_width)
+            height = int(arg_height)
+            if get_proportions(width, height) != proportion:
+                print('Пропорции отличны от оригинала!')
 
-    if arg_width and not arg_height:
-        width = int(arg_width)
-        height = width * proportion
+        elif arg_width and not arg_height:
+            width = int(arg_width)
+            height = width * proportion
 
-    if arg_height and not arg_width:
-        height = int(arg_height)
-        width = height * proportion
+        elif arg_height and not arg_width:
+            height = int(arg_height)
+            width = height * proportion
+
+    except ValueError:
+        sys.exit('Не удалось распознать параметры!')
 
     width = int(width)
     height = int(height)
-
 
     if args.output:
         output_path = args.output
     else:
         output_path = get_output_path(
             input_path,
-            '__{0}X{1}'.format(width,height)
-        )
+            '__{0}X{1}'.format(width, height))
 
     processed_image = resize_image(opened_image, width, height)
 
     save_image(processed_image, output_path)
+
+    print('Файл доступен по пути {}'.format(output_path))
